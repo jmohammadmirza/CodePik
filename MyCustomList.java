ca6958d6cfdc45b304321f6b9e85cc3f26310363
Mohammad Javed <mdaslameme@gmail.com>
2016-10-18 18:52:22 +0530

    Create MyCustomList.java

---
+++ MyCustomList.java
@@ -0,0 +1,99 @@
+Requirement 
+
+Create a Java Class that extend the List Interface / ArrayList Class.
+The Custom List - MyList will allow only 5 elements to be inserted and will not allow null to be inserted.
+@Author Mohammad Javed , India
+*******************************************************************************************************************
+
+
+import java.util.*;
+public  class MyList<T>  extends ArrayList<Object>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return super.size();
+	}
+
+	@Override
+	public boolean add(Object e) {
+		// TODO Auto-generated method stub
+		boolean flag=false;
+		try{
+		if (e!=null && size()< 5){
+			flag=super.add(e);
+		}
+		else{
+			if (!(e!=null))
+			throw new RuntimeException ("MyList-Exception : Null Value not allowed  !");
+			else if(size()>=5)
+		    throw new RuntimeException ("MyList-Exception : List Size exceeded. Only 5 elements allowed !");
+			//System.out.println("Error: List Size excceded or Trying to Insert null");
+		}
+		}
+		catch(Exception e1){
+			e1.printStackTrace();
+			System.exit(0);
+		}
+		return flag;
+	}
+
+	@Override
+	public void add(int index, Object element) {
+		// TODO Auto-generated method stub
+		try{
+		if (element!=null && index<5)
+		super.add(index, element);
+	  else {
+		  
+		  if (!(element!=null))
+				throw new RuntimeException ("MyList-Exception : Null value not allowed  !");
+				else if(index>=5)
+			    throw new RuntimeException ("MyList-Exception : List index exceeded. Only 5 elements allowed !");
+		      
+	    }
+		}
+		catch(Exception e3){
+			e3.printStackTrace();
+			System.exit(0);
+		}
+		
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MyList ml =new MyList();
+		MyList m2 =new MyList();
+		
+		ml.add(1);
+		ml.add(2);
+		ml.add(3);
+		ml.add(4);
+		//Null Test Comment below and check
+		ml.add(null);
+		//
+		ml.add(5);
+	   // Size Test Comment below and check
+		ml.add(6);
+		
+		//add(index,element) Comment below and check
+		m2.add(1,null);
+		// Index Test Comment below and check
+		m2.add(6,8);
+				
+		
+		//Print the List values 
+		 System.out.println("\nList Elements are \n"+Arrays.toString(ml.toArray()));     
+        
+   
+	}
+
+}
